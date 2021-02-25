0d061601c6d9e60669ac3168addbd3c51f91aa62
Juan Sebastian Pardo <78055237+juanspardor@users.noreply.github.com>
2021-02-24 23:17:11 -0500

    Adicion de los diferentes menus en view

--- src/view/View.java
+++ src/view/View.java
@@ -12,13 +12,32 @@ public class View
 	    	
 	    }
 	    
-		public void printMenu()
+	    public void printMenu()
 		{
 			System.out.println("1. Carga la fuente de datos de los videos a una lista encadenada");
 			System.out.println("2. Carga la fuente de datos de los videos a un arreglo dinamico");
 			System.out.println("3. Exit");
 			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return: (e.g., 1):");
 		}
+	    
+	    public void printMenu2()
+		{
+			System.out.println("Ingrese la muestra de los videos cargados");
+			System.out.println("0. Exit");
+			System.out.println("Dar un número mayor a cero para el tamaño de la lista de la muestra de videos cargada, teclear 0 si quiere exit , luego oprimir tecla Return: (e.g., 135):");		
+		}
+	    
+	    public void printMenu3()
+		{
+			System.out.println("El algoritmo a aplicar en la muestra de los videos es:");
+			System.out.println("1. Isertion sort");
+			System.out.println("2. Shell sort");
+			System.out.println("3. Merge sort");
+			System.out.println("4. Quick sort");
+			System.out.println("5. Exit");
+			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return: (e.g., 1):");		
+		}
+
 
 		public void printMessage(String mensaje) {
 
