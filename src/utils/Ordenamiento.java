06b4adf8b6c91049aa980eb0608b95a884acaf3b
Juan Sebastian Pardo <78055237+juanspardor@users.noreply.github.com>
2021-02-26 18:55:53 -0500

    Creacion utils y declaraciones de metodos de ordenamiento

---
+++ src/utils/Ordenamiento.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.Comparator;
+
+import model.data_structures.ILista;
+
+public class Ordenamiento<T extends Comparable<T>>
+{
+
+	public void ordenarInsecion(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
+	{
+		
+	}
+	
+	public void ordenarShell(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
+	{
+		
+	}
+	
+	
+	public void ordenarQuickSort(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
+	{
+		
+	}
+	
+	public void ordenarMerge(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
+	{
+		
+	}
+}
