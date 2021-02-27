73666951cb4a6898b2b693ae0576570cd30bfe03
Juan Sebastian Pardo <78055237+juanspardor@users.noreply.github.com>
2021-02-26 19:02:56 -0500

    Implementacion Insertion y Shell

--- src/utils/Ordenamiento.java
+++ src/utils/Ordenamiento.java
@@ -9,12 +9,54 @@ public class Ordenamiento<T extends Comparable<T>>
 
 	public void ordenarInsecion(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
 	{
-		
+		for(int i = 2; i<=lista.size();i++)
+		{
+			boolean enPosicion = false;
+			for(int j = i; j>1 && !enPosicion; j--)
+			{
+				int comparacion = (ascendente?1:-1)*criterio.compare(lista.getElement(j), lista.getElement(i));
+				if(comparacion<0)
+				{
+					lista.exchange(j, j-1);
+				}
+				else
+				{
+					enPosicion = true;
+				}
+			}
+		}
 	}
 	
 	public void ordenarShell(ILista<T> lista, Comparator<T> criterio, boolean ascendente)
 	{
+		int n = lista.size();
+		int h = 1;
 		
+		while(h<n/3)
+		{
+			h=3*1+1;
+		}
+		
+		while(h>=1)
+		{
+			for(int i = h+1;i<n;i++)
+			{
+				boolean enPosicion = false;
+				for(int j = i;j>h && !enPosicion; j++)
+				{
+					int comparacion = (ascendente?1:-1)* criterio.compare(lista.getElement(j), lista.getElement(i));
+					if(comparacion<0)
+					{
+						lista.exchange(j, j-h);
+					}
+					else
+					{
+						enPosicion = true;
+					}
+				}
+			}
+			h/=3;
+		}
 	}
 	
 	
